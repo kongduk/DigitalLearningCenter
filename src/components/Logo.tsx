3562e3c7eb35ae14001f1a75e8bfad3f08dcaa6b
kongduk <gongduk4960@gmail.com>
2025-04-26 23:04:19 +0900

    first

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo: React.FC = () => {
   return (
     <div className={styles.logo}>
       <Droplets size={28} className={styles.icon} />
-      <span className={styles.text}>디지털뱅킹센터</span>
+      <span className={styles.text}>디지털배움터</span>
     </div>
   );
 };
